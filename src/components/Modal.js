cdaa89ce35d49203e60597c4de08790cce71234b
lukabelani <luka.belani@gmail.com>
2020-10-15 11:46:54 +0200

    add styling and make modal reusable

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
-const Modal = (props) => {
+const Modal = ({ title, content, actions }) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">adfhds sfsghshfs</div>
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{title}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
+      </div>
     </div>,
     document.querySelector("#modal")
   );
